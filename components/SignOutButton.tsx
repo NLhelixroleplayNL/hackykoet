cbadfeecf6d964d8dc3f7c91ec60d456348791bf
Mitch B <hackygbt@gmail.com>
2026-04-24 02:08:56 +0200

    Create SignOutButton.tsx

---
+++ components/SignOutButton.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { signOut } from "next-auth/react"
+
+export default function SignOutButton({ className }: { className?: string }) {
+  return (
+    <button onClick={() => signOut({ callbackUrl: "/" })} className={className}>
+      Sign out
+    </button>
+  )
+}
