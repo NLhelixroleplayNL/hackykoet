6e3ef8b0fa2dbc7b3e7a2e17c53c795a1510f942
Mitch B <hackygbt@gmail.com>
2026-04-24 14:37:40 +0200

    Update AuthSessionProvider.tsx

--- components/AuthSessionProvider.tsx
+++ components/AuthSessionProvider.tsx
@@ -1,6 +1,16 @@
 "use client"
+
 import { SessionProvider } from "next-auth/react"
 
-export default function AuthSessionProvider({ children }: { children: React.ReactNode }) {
+/**
+ * Thin client wrapper around NextAuth's SessionProvider.
+ * Required so that client components can call useSession().
+ * Server components should use getServerSession(authOptions) directly.
+ */
+export default function AuthSessionProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return <SessionProvider>{children}</SessionProvider>
 }
